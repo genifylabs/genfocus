bb8937964d8b8c31898ae0f004238d6a63efa122
genifylabs <teamgenify@gmail.com>
2026-06-03 13:05:30 +0200

    Add files via upload

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,10 +4,10 @@
  * or configure it directly in the app's Settings page.
  */
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAHIG8JMCEUdqd21p2esXcUcFieqntQGZ0",
-  authDomain: "genify-4f779.firebaseapp.com",
-  projectId: "genify-4f779",
-  storageBucket: "genify-4f779.firebasestorage.app",
-  messagingSenderId: "534551060898",
-  appId: "1:534551060898:web:c8dab93e44ee3c0126d2fb",
+  apiKey: "AIzaSyDQQUkjrXygqG2oE_OlNZZ68eoLBbOohp0",
+  authDomain: "genfocus-35d2e.firebaseapp.com",
+  projectId: "genfocus-35d2e",
+  storageBucket: "genfocus-35d2e.firebasestorage.app",
+  messagingSenderId: "263069806851",
+  appId: "1:263069806851:web:168a63a52de617d51cf936"
 };
